5d4d750013c362e0418906cc50253f586ab04297
Monica Aragon <maragojica@gmail.com>
2025-12-10 20:37:21 -0500

    Organized Intelligence Styles and Scripts

--- INtelligence HTML Base/newsletters.php
+++ INtelligence HTML Base/newsletters.php
@@ -10,7 +10,7 @@
 		</section>		
 	
 		<section class="article-content section-padding-large">	
-		<div class="article-img contain-padding pt-0 pb-0">
+		<div class="article-img contain-padding pt-0">
 			<div class="container">
 				<img src="assets/img/article-img.jpg" alt="Article image" class="img-fluid">				
 			</div>
@@ -18,29 +18,68 @@
 		
 		<div class="article-text font-14 contain-padding pt-0 pb-0">
 			<div class="container">
-				<h2>H2 nulla suspendisse mi leo aliquet.</h2>			
+				<h2 class="pt-0 text-center-mobile">H2 nulla suspendisse mi leo aliquet.</h2>			
 				<p>Lorem ipsum dolor sit amet consectetur. Facilisis vitae leo pharetra pellentesque vulputate placerat non faucibus ipsum. Sagittis arcu quam pretium orci potenti sodales lectus tellus. Ligula eu mollis feugiat ultrices fringilla at maecenas. Malesuada tempor ipsum urna dictum platea elementum. Mattis accumsan turpis odio purus in neque morbi augue nulla. Diam sit malesuada hac ut.</p>
-				
-		</div>
-
+			</div>
+      </div>
 		<div class="article-newsletter contain-padding">
-			
-			<div class="list-item">
-				<div class="item-header">
-					<div class="title-wrapper">
-						<div class="avatar"></div>
-						<h3 class="item-title">H3 Weekly digest</h3>
+			<div class="container">
+				<div class="items-newsletters section-padding-large">
+					<div class="list-item-newsletter">
+					<div class="item-header-newsletter">
+						<div class="title-wrapper-newsletter">
+							<div class="card-author mb-0"><img src="assets/img/profile.png" alt="author" class="author-img" width="46"></div>
+							<h3 class="item-title-newsletter">H3 Weekly digest</h3>
+						</div>
+						<a href="#" class="preview-btn">
+							Preview
+							<svg class="preview-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
+							</svg>							
+						</a>
 					</div>
-					<a href="#" class="preview-btn">
-						Preview
-						<svg class="preview-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
-						</svg>
-					</a>
+					<p class="item-description font-14">
+						Lorem ipsum dolor sit amet consectetur. Facilisis vitae leo pharetra pellentesque vulputate placerat non faucibus ipsum. Sagittis arcu quam pretium orci potenti sodales lectus tellus. Ligula eu mollis feugiat ultrices fringilla et maecenas.
+					</p>
 				</div>
-				<p class="item-description">
-					Lorem ipsum dolor sit amet consectetur. Facilisis vitae leo pharetra pellentesque vulputate placerat non faucibus ipsum. Sagittis arcu quam pretium orci potenti sodales lectus tellus. Ligula eu mollis feugiat ultrices fringilla et maecenas.
-				</p>
+				<div class="list-item-newsletter">
+					<div class="item-header-newsletter">
+						<div class="title-wrapper-newsletter">
+							<div class="card-author mb-0"><img src="assets/img/profile.png" alt="author" class="author-img" width="46"></div>
+							<h3 class="item-title-newsletter">H3 Cobus van Staden</h3>
+						</div>
+						<a href="#" class="preview-btn">
+							Preview
+							<svg class="preview-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
+							</svg>							
+						</a>
+					</div>
+					<p class="item-description font-14">
+						Lorem ipsum dolor sit amet consectetur. Facilisis vitae leo pharetra pellentesque vulputate placerat non faucibus ipsum. Sagittis arcu quam pretium orci potenti sodales lectus tellus. Ligula eu mollis feugiat ultrices fringilla et maecenas.
+					</p>
+				</div>
+				<div class="list-item-newsletter">
+					<div class="item-header-newsletter">
+						<div class="title-wrapper-newsletter">
+							<div class="card-author mb-0"><img src="assets/img/profile.png" alt="author" class="author-img" width="46"></div>
+							<h3 class="item-title-newsletter">H3 Critical Minerals</h3>
+						</div>
+						<a href="#" class="preview-btn">
+							Preview
+							<svg class="preview-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
+							</svg>							
+						</a>
+					</div>
+					<p class="item-description font-14">
+						Lorem ipsum dolor sit amet consectetur. Facilisis vitae leo pharetra pellentesque vulputate placerat non faucibus ipsum. Sagittis arcu quam pretium orci potenti sodales lectus tellus. Ligula eu mollis feugiat ultrices fringilla et maecenas.
+					</p>
+				</div>
+				</div>		
+				<div class="box-subscribe">
+					<img src="assets/img/subscribe.png" alt="Subscribe" class="img-fluid w-100">
+				</div>		
 			</div>
 		</div>
 	</section>
